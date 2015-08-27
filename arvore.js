8e01a210f74bccdb5d2cb68094f6609ff4254564
Jedaias Rodrigues <Jedarc@users.noreply.github.com>
2015-08-27 10:23:07 -0300

    Indentação

--- arvore.js
+++ arvore.js
@@ -25,22 +25,22 @@
 		this.Adicionar = function(valor){
 			
 			var ctx = canvas.getContext("2d");
-            var w = ctx.measureText(valor).width + 10;
-          
-            ctx.beginPath();
-            ctx.fillStyle = "rgb(200,0,0)";
-            
-            var xArco = (x/2);
-            var yArco = (y/2);
-            
-            ctx.arc(xArco, yArco, w/2, 0, 2 * Math.PI, false);
-            ctx.fill();
-
-            ctx = canvas.getContext("2d");
-            ctx.font = '12px Arial';
-            ctx.fillStyle = 'white';
-            ctx.textAlign = 'center';
-            ctx.fillText(valor, xArco, yArco+3);
+	            	var w = ctx.measureText(valor).width + 10;
+	          
+	            	ctx.beginPath();
+	            	ctx.fillStyle = "rgb(200,0,0)";
+	            
+	            	var xArco = (x/2);
+	            	var yArco = (y/2);
+	            
+	            	ctx.arc(xArco, yArco, w/2, 0, 2 * Math.PI, false);
+	            	ctx.fill();
+	
+	            	ctx = canvas.getContext("2d");
+	            	ctx.font = '12px Arial';
+	            	ctx.fillStyle = 'white';
+	            	ctx.textAlign = 'center';
+	            	ctx.fillText(valor, xArco, yArco+3);
 			
 		};
 		
